b9bbd5772e0566f4b1461078c50b0d28cf9714e7
robertogzz10 <you@example.com>
2019-09-26 14:09:34 -0500

    agregue carpeta

---
+++ Roberto_Israel_Gonzalez_Escamilla/clase.js
@@ -0,0 +1,27 @@
+let entero = 20;
+let letra = 'b';
+let palabra1 = "renglon";
+let decimal = 3.1029;
+let booleano = true;
+
+console.log(
+    entero,
+    letra,
+    palabra1,
+    decimal,
+    booleano
+);
+
+let entero2 = 10;
+let letra2 = 'c';
+let palabra2 = "renglon2";
+let decimal2 = 2.598;
+let booleano2 = true;
+
+console.log(
+    entero2,
+    letra2,
+    palabra2,
+    decimal2,
+    booleano2
+);
