b1eac7d4a92f935a1ecd4cf12b824e50ba542d32
JACC7 <albertofenix_2001@outlook.com>
2019-09-26 14:07:18 -0500

    XML de Pokemon y Python

---
+++ Jesus_Alberto_Castro_Carcano/python.py
@@ -0,0 +1,12 @@
+edad = 17;
+año = 2019;
+letra1 = 'a';
+letra2 = 'b';
+texto1 = "hola";
+texto2 = "no se";
+gravedad = 9.8;
+dolar = 18.74;
+hombre = True;
+joven = True;
+
+print(edad, año, letra1, letra2, texto1, texto2, gravedad, dolar, hombre, joven);
