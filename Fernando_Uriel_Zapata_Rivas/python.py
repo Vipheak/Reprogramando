64534a19dfd932842f6535a5eaf44963cda22bfe
fernando109 <uriel109.ur@gmail.com>
2019-09-26 14:04:41 -0500

    Clase de python

---
+++ Fernando_Uriel_Zapata_Rivas/python.py
@@ -0,0 +1,25 @@
+mes = 9;
+edad = 20;
+letra1 = 'c';
+letra2 = 'd';
+nombre = "fernando";
+animal = "leon";
+gravedad = 9.8;
+euro = 21.44;
+mexico = True;
+hombre = True;
+
+print(
+    mes,
+    edad,
+    letra1,
+    letra2,
+    nombre,
+    animal,
+    gravedad,
+    euro,
+    mexico,
+    hombre,
+);
+
+
