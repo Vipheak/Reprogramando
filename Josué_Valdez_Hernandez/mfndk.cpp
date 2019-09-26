4931fb23d0d366f98547f7a5c7c0457490752cae
Your Name <josue.dil@hotmail.com>
2019-09-26 16:55:28 -0500

    qwerty

---
+++ Josué_Valdez_Hernandez/mfndk.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {                        
+    int biologia = 70; 
+    int b = 1;
+    float x = 0.25, y = 0.50, z = 0.75;
+    char nombreVar = 'v';
+    char tengohambre = "!";
+    bool kckjdn = true; 
+    bool edkmw = false;
+    string ola = "adldsaldasdasdasd";
+    string alo = "dsadsadsalasdlda";
+
+    cout << biologia << endl 
+         << b << endl 
+         << x << endl 
+         << y << endl 
+         << z << endl
+         << nombreVar << endl
+         << tengohambre << endl 
+         << kckjdn << endl 
+         << edkmw << endl 
+         << ola << endl 
+         << alo << endl;
+}
