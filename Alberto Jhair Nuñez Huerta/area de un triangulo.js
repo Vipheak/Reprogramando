810d92593db2ecd041ca23ce848923f5bf33849a
Jhair374 <jhairhuerta01@gmail.com>
2019-10-01 15:17:32 -0500

    cambios

---
+++ Alberto Jhair Nuñez Huerta/area de un triangulo.js
@@ -0,0 +1,7 @@
+var base = 5;
+var altura = 4;
+var area = 0;
+
+area = (base*altura)/2;
+
+console.log(area);
---
+++ Alberto Jhair Nuñez Huerta/declaracion de variables.js
@@ -0,0 +1,15 @@
+var ano = 9;
+var mes = 'j';
+var palabra = "hola";
+var dia = 4;
+var codigo = true;
+
+let estaVariableEsCamelCase = 30;
+
+console.log(
+  ano,
+  mes,
+  palabra,
+  dia,
+  codigo
+);
