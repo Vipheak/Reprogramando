f12bc4ae07b9b453152473861b48df358a177a96
ArnoldoParas <aparasreyes2@gmail.com>
2019-10-01 14:37:39 -0500

    Archivos en JavaScript

---
+++ Mi carpeta AP/Codigo_JavaScript.js
@@ -0,0 +1,5 @@
+let palabra_1 = "Arturo";
+let palabra_2 = "estuvo";
+let palabra_3 = "aqui";
+
+console.log(palabra_1+" "+palabra_2+" "+palabra_3);
---
+++ Mi carpeta AP/Declaracion_de_variables.js
@@ -0,0 +1,16 @@
+var edad = 15;
+var nombre = 'Arnoldo';
+var mensaje = "Hello World";
+var horas_dormidas = 5;
+var paso_examenes = true;
+
+let estaVariableEsCamelCase = 20;
+
+console.log
+(
+  edad,
+  nombre,
+  mensaje,
+  horas_dormidas,
+  paso_examenes
+);
