65eb942820235e6f6a311889895aa1c0ddbd1320
ArnoldoParas <aparasreyes2@gmail.com>
2019-10-22 21:25:00 -0500

    Expeimento de namespace

---
+++ Mi carpeta AP/Exeprimento_de_Namespace.js/Main.js
@@ -0,0 +1,14 @@
+const Archivo1 = require('./Archivo1.js');
+const Archivo2 = require('./Archivo2.js');
+const Archivo3 = require('./Archivo3.js');
+const Archivo4 = require('./Archivo4.js');
+
+console.log(
+  Archivo1.titulo,
+  "\n",
+  Archivo2.introduccion,
+  "\n",
+  Archivo3.desarrollo,
+  "\n",
+  Archivo4.conclusion
+);
