02ae51a2e61b8aedbb11f38e5fe927e997d144ec
erickbgc <erick.brmgc@gmail.com>
2019-10-23 23:18:20 -0500

    Ejercicios funciones

---
+++ Erick_Brandon_Garcia_Castillo/ejercicios_funciones01.py
@@ -0,0 +1,12 @@
+def solucion(operacion):
+    if(operacion==True):
+        print('Respuesta correcta!')
+    else:
+        print('Respuesta equivocada')
+
+    return operacion
+
+
+ecuacion = 1/2
+
+solucion(ecuacion)
