a368ccfdf806b69fae2c3d3049da84f941961801
LM <luismcortez73@gmail.com>
2019-11-10 13:48:15 -0600

    mysql :)

---
+++ Luis_Martin_Cortez_Requenes/MYSQL.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Dungeon {
+
+  PersonajeID int,
+  Nombre           varchar(255),
+  Apellido         varchar(255),
+  Origen           varchar(255),
+  Historia         varchar(255),
+  Habilidad        varchar(255),
+  Poder            varchar(255),
+  Especializacion  varchar(255),
+  Tecnicas         varchar(255),
+  E.S.P.E.C.I.A.L. varchar(255)
+
+};
+
+  /* TABLA DE REFERENCIA PARA MYSQL,      /
+ /   PROTOTIPO DE CREACION DE PERSONAJE */
